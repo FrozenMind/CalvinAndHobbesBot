e9b684a12cc0255e562b913ac070b367d51647a2
Valentin Rebscher <valentin.rebscher@live.de>
2017-09-07 18:21:48 +0200

    user json file to save users

--- bot.js
+++ bot.js
@@ -1,96 +1,89 @@
-var request = require("request");
-var cheerio = require("cheerio");
-var TelegramBot = require('node-telegram-bot-api');
-var fs = require('fs');
-var bot;
-var users = [];
-var token;
-var inter;
+var request = require("request")
+var cheerio = require("cheerio")
+var TelegramBot = require('node-telegram-bot-api')
+var fs = require('fs')
+var bot
+var users = [] //user array
+var token //telegram bot api token
+var inter //interval to check if pic needs to be send
 
-readUsers();
-initBot();
-startInterval();
+readUsers()
+initBot()
+startInterval()
 
 //initialite telegram bot
 function initBot() {
   bot = new TelegramBot(process.env.CAH_BOT, { //token is read from environment
     polling: true
-  });
+  })
   bot.on('message', (msg) => {
     switch (msg.text.split(' ')[0]) {
       case "time": //sets the time the user want to receive the img
-        //save user with time to config file
-        var save = msg.chat.id + "_" + msg.text.replace(/\s+/g, '').replace('time', '') + "\n";
-        //add to file
-        fs.appendFile(__dirname + '/user.config', save, function(err) {
-          if (err) {
-            console.log(err);
-          } else {
-            // done
+        //check if user exist
+        var userFound = false
+        //get the time out of the message
+        var newTime = msg.text.replace(/\s+/g, '').replace('time', '')
+        for(var u = 0; u < users.length; u++){
+          //if user found update his time
+          if(users[u].id == msg.chat.id){
+            userFound = true            
+            users[u].time = newTime
+            console.log("update time for " + msg.chat.id + " to " + newTime)
           }
-        });
-        //read user again
-        readUsers();
+        }
+        //if user not found in array, add new one
+        if(!userFound){
+          var newUser = {
+            id: msg.chat.id,
+            time: newTime
+          }
+          users.push(newUser)
+          console.log("added new user: " + JSON.stringify(newUser))
+        }
+        //update user file
+        fs.writeFileSync(__dirname + '/user_config.json', users)
         break;
     }
-    console.log(msg.chat.id);
-  });
+  })
 }
 
-//get image by date
-function getImage(date, id) {
-  var year = date.getFullYear();
-  var month = date.getMonth() + 1;
-  var day = date.getDate();
+//get image for the current day and send it to the user
+function sendImage(date, id) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
 
   request({
     uri: "http://www.gocomics.com/calvinandhobbes/" + year + "/" + month + "/" + day,
   }, function(error, response, body) {
-    var $ = cheerio.load(body);
+    var $ = cheerio.load(body)
 
     $('img').each(function(i, ele) {
       var link = $(this).attr('src');
       if (link.startsWith('http://assets')) {
         console.log(link);
-        sendImg(id, link);
+        //send pic to user
+        bot.sendPhoto(id, link);
       }
-    });
-  });
+    })
+  })
 }
 
-//send Img to user
-function sendImg(id, url) {
-  bot.sendPhoto(id, url);
-}
-
+//read users out of file and load to useres object
 function readUsers() {
-  users = [];
-  fs.readFile(__dirname + '/user.config', (err, data) => {
-    if (!err) {
-      lines = data.toString().split('\n');
-      for (i = 0; i < lines.length; i++) {
-        if (lines[i] != "") {
-          cline = lines[i].split('_')
-          users.push({
-            id: cline[0],
-            time: parseInt(cline[1])
-          });
-        }
-      }
-    } else {
-      console.log("err");
-    }
-    console.log(users);
-  });
+  users = []
+  users = fs.readFileSync(__dirname + '/user_config.json')
+  console.log(users)
 }
 
 //check everyHour if a user wants a pic
+//TODO: check every min, so its to 1 minute exactly, and find a way to save if user got the picture already today.
 function startInterval() {
   inter = setInterval(function() {
     var d = new Date();
     for (i = 0; i < users.length; i++) {
       if (users[i].time == d.getHours())
-        getImage(d, users[i].id)
+        sendImage(d, users[i].id)
     }
   }, 3600000); //3600000 = 1 hour
 }
